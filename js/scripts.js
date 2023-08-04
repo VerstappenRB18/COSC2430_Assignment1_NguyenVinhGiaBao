a44a9de239c6094bfd65802b58a54aa555985b1e
Vinh Gia Bao Nguyen <s3986287@rmit.edu.vn>
2023-08-04 18:28:08 +0700

    publish materials to main

---
+++ js/scripts.js
@@ -0,0 +1,54 @@
+/*  
+    RMIT University Vietnam
+    Course: COSC2430 Web Programming
+    Semester: 2023B
+    Assessment: Assignment 1
+    Author: Nguyen Vinh Gia Bao
+    ID: s3986287
+    Acknowledgement: 
+    https://www.amazon.com/s?bbn=283155&rh=n%3A283155&dc&qid=1689659947&ref=lp_10898_ex_n_1
+    https://www.canva.com/
+    https://www.youtube.com/watch?v=bk3Y4heVdFs&ab_channel=Mr.WebDesigner
+    https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+    https://www.30secondsofcode.org/css/s/hover-underline-animation/
+    https://www.bookswagon.com
+    https://www.thriftbooks.com
+    https://unsplash.com/photos/xY55bL5mZAM
+*/
+
+
+let mybutton = document.getElementById("top-button");
+let footer = document.querySelector("footer");
+
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        mybutton.style.display = "flex";
+    } else {
+        mybutton.style.display = "none";
+    }
+
+    let footerRect = footer.getBoundingClientRect();
+
+    if (footerRect.top < window.innerHeight) {
+        mybutton.style.bottom = (window.innerHeight - footerRect.top + 10) + 'px';
+    } else {
+        mybutton.style.bottom = '10px';
+    }
+}
+
+var bigImg = document.getElementById("big-img");
+var smallImg = document.getElementsByClassName("small-img");
+
+smallImg[0].onclick = function(){
+    bigImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function(){
+    bigImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = function(){
+    bigImg.src = smallImg[2].src;
+}
